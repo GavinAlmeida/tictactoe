41f30f80cc09a4e9baba06cb6b553b07a4e7f843
Gavin Almeida <96859009+GavinAlmeida@users.noreply.github.com>
2024-11-03 20:37:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const tiles = Array.from(document.querySelectorAll('.tile'));
-    const playerDisplay = document.querySelector('.display-player');
-    const resetButton = document.querySelector('#reset');
-    const announcer = document.querySelector('.announcer');
-    let board = ['', '', '', '', '', '', '', '', ''];
-    let currentPlayer = 'X';
-    let isGameActive = true;
+window.addEventListener('DOMContentLoaded', () => { //targets HTML to listen to events on
+    const tiles = Array.from(document.querySelectorAll('.tile')); //tiles
+    const playerDisplay = document.querySelector('.display-player'); //playerdisplay
+    const resetButton = document.querySelector('#reset'); //query selector for reset
+    const announcer = document.querySelector('.announcer'); //announces winner
+    let board = ['', '', '', '', '', '', '', '', '']; //let so that the board positions can change
+    let currentPlayer = 'X'; //X starts
+    let isGameActive = true; //game begins
   
-    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERX_WON = 'PLAYERX_WON'; //for who wins
     const PLAYERO_WON = 'PLAYERO_WON';
-    const TIE = 'TIE';
-    const winningConditions = [
+    const TIE = 'TIE'; //draw
+    const winningConditions = [ //all possible winning conditions for the 3 by 3 board
      [0, 1, 2],
      [3, 4, 5],
      [6, 7, 8],
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
      [0, 4, 8],
      [2, 4, 6]
   ];
-  const isValidAction = (tile) => {
+  const isValidAction = (tile) => { //conditions for x vs o
       if (tile.innerText === 'X' || tile.innerText === 'O'){
           return false;
       }
@@ -28,18 +28,18 @@ window.addEventListener('DOMContentLoaded', () => {
       return true;
   };
   
-  const updateBoard =  (index) => {
+  const updateBoard =  (index) => { //current player is updated and shown after one player goes
      board[index] = currentPlayer;
   }
   
-  const changePlayer = () => {
+  const changePlayer = () => { //player changes after each click, so you cant go more than once in a row
       playerDisplay.classList.remove(`player${currentPlayer}`);
       currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
       playerDisplay.innerText = currentPlayer;
       playerDisplay.classList.add(`player${currentPlayer}`);
   }
   
-  const announce = (type) => {
+  const announce = (type) => { //player winning announces
       switch(type){
          case PLAYERO_WON:
               announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
@@ -53,7 +53,7 @@ window.addEventListener('DOMContentLoaded', () => {
       announcer.classList.remove('hide');
   };
   
-  function handleResultValidation() {
+  function handleResultValidation() { //three in a row wins
     let roundWon = false;
     for (let i = 0; i <= 7; i++) {
       const winCondition = winningConditions[i];
@@ -78,7 +78,7 @@ window.addEventListener('DOMContentLoaded', () => {
     if (!board.includes("")) announce(TIE);
   }
   
-  const userAction = (tile, index) => {
+  const userAction = (tile, index) => { //more updates to board
     if (isValidAction(tile) && isGameActive) {
       tile.innerText = currentPlayer;
       tile.classList.add(`player${currentPlayer}`);
@@ -92,7 +92,7 @@ window.addEventListener('DOMContentLoaded', () => {
       tile.addEventListener('click', () => userAction(tile, index));
   });
   
-  const resetBoard = () => {
+  const resetBoard = () => { //board resets after game is done
       board = ['', '', '', '', '', '', '', '', ''];
       isGameActive = true;
       announcer.classList.add('hide');
